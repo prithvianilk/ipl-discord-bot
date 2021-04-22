953414a904069595a8886ce3d1ae12f4f6d0b90f
Prithvi Anil K <thepirateking3d2y@gmail.com>
2021-04-22 23:19:00 +0530

    added basic bs4 scraping stuff

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
 import discord
+import requests
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv() 
+URL = 'https://www.cricbuzz.com/'
 TOKEN = os.environ['DISCORD_TOKEN']
 
 client = discord.Client()
@@ -17,6 +20,14 @@ async def on_message(message):
         return
 
     if message.content.startswith('$score'):
-        await message.channel.send('Score is your mom!')
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        boll = soup.find(class_ = 'cb-hmscg-bwl-txt')
+        boll_team = boll.find(class_ = 'cb-hmscg-tm-nm').text
+        boll_score = boll.find(style = 'display:inline-block; width:140px').text
+        bat = soup.find(class_ = 'cb-hmscg-bat-txt')
+        bat_team = bat.find(class_ = 'cb-hmscg-tm-nm').text
+        bat_score = bat.find(style = 'display:inline-block; width:140px').text
+        await message.channel.send(bat_team + '\n' + bat_score + '\n' + boll_team + '\n' + boll_score)
 
 client.run(TOKEN)
