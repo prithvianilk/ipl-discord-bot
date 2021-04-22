a89a7a57570e7724ed180c10d2290942d500d1eb
MuthuSubramanian00 <muthusubramanian00@gmail.com>
2021-04-23 00:19:50 +0530

    HELLO

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-import os
-import discord
-import requests
-from bs4 import BeautifulSoup
-from dotenv import load_dotenv
+import os 
+import discord 
+import requests 
+from bs4 import BeautifulSoup 
+from dotenv import load_dotenv 
 
 load_dotenv() 
 URL = 'https://www.cricbuzz.com/'
 TOKEN = os.environ['DISCORD_TOKEN']
 
-client = discord.Client()
+client = discord.Client() 
 
 @client.event
 async def on_ready():
@@ -19,15 +19,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$score'):
-        page = requests.get(URL)
+    if message.content.startswith('$score'): 
+        page = requests.get(URL) 
         soup = BeautifulSoup(page.content, 'html.parser')
-        boll = soup.find(class_ = 'cb-hmscg-bwl-txt')
-        boll_team = boll.find(class_ = 'cb-hmscg-tm-nm').text
-        boll_score = boll.find(style = 'display:inline-block; width:140px').text
-        bat = soup.find(class_ = 'cb-hmscg-bat-txt')
-        bat_team = bat.find(class_ = 'cb-hmscg-tm-nm').text
-        bat_score = bat.find(style = 'display:inline-block; width:140px').text
-        await message.channel.send(bat_team + '\n' + bat_score + '\n' + boll_team + '\n' + boll_score)
+        bowl = soup.find(class_ = 'cb-hmscg-bwl-txt') 
+        bowl_team = bowl.find(class_ = 'cb-hmscg-tm-nm').text 
+        bowl_score = bowl.find(style = 'display:inline-block; width:140px').text 
+        bat = soup.find(class_ = 'cb-hmscg-bat-txt') 
+        bat_team = bat.find(class_ = 'cb-hmscg-tm-nm').text 
+        bat_score = bat.find(style = 'display:inline-block; width:140px').text 
+        await message.channel.send(bat_team + '\n' + bat_score + '\n' + bowl_team + '\n' + bowl_score)
+
 
 client.run(TOKEN)
