fcf9407ef508e2f1012a87c47fa1905ad40c9487
Ganesh G <giriswamyganesh@gmail.com>
2021-04-26 23:17:52 +0530

    change table command to show points next to team

--- src/main.py
+++ src/main.py
@@ -93,8 +93,11 @@ async def on_message(message):
         for i in range(len(table_data)):
             desc = ""
             for j in range(len(table_data[i])):
-                desc = desc + headers[j] + ' : ' + table_data[i][j] + '\n'
-            embedVar_table.add_field(name = str(i + 1) + '. ' + names[i] , value = desc, inline=False)
+                if headers[j] == 'Pts':
+                    continue
+                else:
+                    desc = desc + headers[j] + ' : ' + table_data[i][j] + '\n'
+            embedVar_table.add_field(name = str(i + 1) + '. ' + names[i] + ' : ' + table_data[i][5] + ' points' , value = desc, inline=False)
         embedVar_table.add_field( name = '\u200b' , value = "For more information, visit [criccbuzz]({})".format(TABLE_URL), inline=False)
         await message.channel.send(embed=embedVar_table)
 
