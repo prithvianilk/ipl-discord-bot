bfe7e186bc605d71bdd7c5cca820c57c935e232f
MuthuSubramanian00 <muthusubramanian00@gmail.com>
2021-04-29 22:05:08 +0530

    Table as picture

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 import os
 import discord
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from commands import get_help, get_score, get_table, get_orange_cap, get_purple_cap
 
-# from dotenv import load_dotenv 
-# load_dotenv() 
+from dotenv import load_dotenv 
+load_dotenv() 
 
 TOKEN = os.environ['DISCORD_TOKEN']
-PYTHON_ENV = os.environ['PYTHON_ENV'] # Can be 'dev' or 'prod'
-PREFIX = '${}'.format('dev-' if PYTHON_ENV == 'dev' else '') # Must be added to every 'starts with' command case
+PYTHON_ENV = os.environ['PYTHON_ENV'] 
+PREFIX = '${}'.format('dev-' if PYTHON_ENV == 'dev' else '') 
+
+PATH = "{}chromedriver.exe".format('./' if PYTHON_ENV == 'prod' else 'C:\Program Files (x86)\\') 
+driver = webdriver.Chrome(PATH) 
+driver.get("https://www.iplt20.com/points-table/men/2021") 
+driver.maximize_window()
+tag = driver.find_element_by_tag_name("body")
+
+element = driver.find_element_by_id("main-content")
+driver.execute_script("window.scrollTo(0, {})".format(element.location['y'] - 190))
 
 client = discord.Client() 
 
@@ -27,7 +38,9 @@ async def on_message(message):
         await message.channel.send(embed = get_score())
 
     if message.content.startswith(PREFIX + 'table'):
-       await message.channel.send(embed = get_table())
+        get_table(driver)
+        await message.channel.send(file=discord.File("table.png"))
+        os.remove("table.png")
 
     if message.content.startswith(PREFIX + 'orange-cap'):
         await message.channel.send(embed = get_orange_cap())
